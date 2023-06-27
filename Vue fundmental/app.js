7e08b81b8e669ef41cff91369da2bb52c1eda64f
hamdymohamed162002 <73902474+hamdymohamed162002@users.noreply.github.com>
2023-06-27 16:15:10 +0300

    event listener

--- Vue fundmental/app.js
+++ Vue fundmental/app.js
@@ -3,9 +3,17 @@ data(){
     return{
         firstName: "hamdy",
         lastName: "mohamed",
-        url:'https://www.google.com/'
+        url:'https://www.google.com/',
+        age:20
     }
 }
-
-})
+,
+methods:{
+    increment(){
+        this.age++
+    }
+    ,decrement(){
+        this.age--
+    }
+}})
 .mount('#app')
