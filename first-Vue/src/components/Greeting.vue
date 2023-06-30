34ade4c2f62d46c865e6ef018322b65c2128baaa
hamdymohamed162002 <73902474+hamdymohamed162002@users.noreply.github.com>
2023-06-30 23:40:48 +0300

    props

---
+++ first-Vue/src/components/Greeting.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+{{ greeting }}
+    </div>
+</template>
+
+<script >
+export default {
+    name: 'Greeting',
+  data() {
+    return {
+      greeting: 'Hello World!'
+    }
+  }
+}
+</script>
+<style scoped>
+div {
+  color: red;
+}
+</style>
+
---
+++ first-Vue/src/components/User.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+<button @click="addAge">add</button>
+
+<p>your age is {{ age }}</p>
+    </div>
+</template>
+
+<script >
+export default{
+    name: 'User',
+    props:['age'],
+    methods:{
+        addAge(){
+            this.$emit('addAge',2);
+        }
+    }
+}
+
+</script>
+
